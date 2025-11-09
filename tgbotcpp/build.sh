ba40e8a5f8dd251ab7d082d97c1f220d812feb80
Wolfdale27 <wolfdale27@yandex.ru>
2025-11-09 21:35:09 +0300

    feat: fully cross-compile tgbotcpp

---
+++ tgbotcpp/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Before download this repository and add its recipes to Conan
+#git clone https://github.com/tttapa/docker-arm-cross-toolchain.git
+#conan remote add tttapa-docker-arm-cross-toolchain ./docker-arm-cross-toolchain
+
+conan install . --output-folder=build --build=missing --profile:build=default --profile:host=./aarch64-rpi3-linux-gnu
+cmake --build --parallel --preset conan-release
+cmake --preset conan-release --fresh
