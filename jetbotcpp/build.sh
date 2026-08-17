9bd191bf4f979df6a2969134dbbf55c8776348ce
jetfire27 <wolfdale27@yandex.ru>
2026-08-17 08:53:21 +0300

    feat: trying cross-compile for rpi zero 2wcd

--- jetbotcpp/build.sh
+++ jetbotcpp/build.sh
@@ -5,5 +5,5 @@
 #conan remote add tttapa-docker-arm-cross-toolchain ./docker-arm-cross-toolchain
 
 conan install . --output-folder=build --build=missing --profile:build=default --profile:host=./aarch64-rpi3-linux-gnu
-cmake --build --parallel --preset conan-release
+cmake --build --parallel --preset conan-release # тут ошибка
 cmake --preset conan-release --fresh
